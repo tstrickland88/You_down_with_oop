51d89135cf14361c4c3ee2858d4dc3894f23b7b4
tstrickland88 <77135762+tstrickland88@users.noreply.github.com>
2021-06-29 22:07:03 -0700

    imported classes to index.js file

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const Manager=require("./lib/Manager.js")
 const Engineer=require("./lib/Engineer.js")
-const intern=require("./lib/Intern.js")
+const Intern=require("./lib/Intern.js")
+
+const inquirer=require("inquirer")
+const fs=require("fs")
+const path= require("path")
+
+const outputDir=path.resolve(__dirname, "output")
+const outputFile=path.join(outputDir,"output.html")
+
+var team=[]
